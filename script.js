dce0ad9ec4522b7641c6e09484a07a00a24b02c9
valentinyak <63512456+valentinyak@users.noreply.github.com>
2022-05-10 22:24:48 +0300

    add js code and trasition to open/close buttons

---
+++ script.js
@@ -0,0 +1,7 @@
+const article = document.querySelector('.article');
+const circle = document.querySelector('.circle');
+
+circle.addEventListener('click', () => {
+  circle.classList.toggle('active');
+  article.classList.toggle('active');
+});
