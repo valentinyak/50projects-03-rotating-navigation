08f950eb79afe8851c4edc509fec7fad214c4bda
valentinyak <63512456+valentinyak@users.noreply.github.com>
2022-05-24 21:23:24 +0300

    add rotation to nav

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const article = document.querySelector('.article');
 const circle = document.querySelector('.circle');
+const navList = document.querySelector('.nav-list');
 
 circle.addEventListener('click', () => {
   circle.classList.toggle('active');
   article.classList.toggle('rotated');
+  navList.classList.toggle('opened');
 });
